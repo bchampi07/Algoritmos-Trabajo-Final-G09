30dda07c8531d4bd400dfe201d72f288ee6dac05
sergiohurtadoramos <sergio.hurtado@estudiante.ucsm.edu.pe>
2026-06-25 13:32:49 -0500

    LinkedBST.java

--- BST.java
+++ BST.java
@@ -1,1 +1,25 @@
 
+
+
+public Class LinkedBST<T extends Comparable<T>> {
+  private Node<T> root;
+  public LinkedBST() {
+    this.root = null;
+  }
+
+  public Node<T> getRoot()
+    return this.root;
+}
+
+public T search(T d) throws ItemNoFound {
+  Node<T> resultado = searchRec(this.root, d);
+
+  if (resultado == null) {
+    throw new ItemNoFound();
+  }
+
+  return resultado.data;
+}
+
+public Node<T>
+    
