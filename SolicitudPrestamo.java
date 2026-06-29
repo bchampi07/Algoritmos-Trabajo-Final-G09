0a9e08824cb04efb4a7d2a0b55924de1bd3b469b
Braly Champi <braly.champi@ucsm.edu.pe>
2026-06-28 23:09:13 -0500

    Creando clase SolicitudPrestamo
    Added a class for loan requests with student details and book information.

---
+++ SolicitudPrestamo.java
@@ -0,0 +1,25 @@
+public class SolicitudPrestamo {
+  private String codigoEstudiante;
+  private String nombreEstudiante;
+  private int codigoLibro;
+  private String fechaSolicitud;
+
+  public SolicitudPrestamo(String codigoEstudiante, String nombreEstudiante, int codigoLibro, String fechaSolicitud) {
+    this.codigoEstudiante = codigoEstudiante;
+    this.nombreEstudiante = nombreEstudiante;
+    this.codigoLibro = codigoLibro;
+    this.fechaSolicitud = fechaSolicitud;
+  }
+
+  public int getCodigoLibro() {
+    return codigoLibro;
+  }
+
+  @override
+  pubic String toString(){
+    return "Estudiante: " + codigoEstudiante +
+            "Nombre: " + nombreEstudiante + 
+            "Libro solicitado: " + codigoLibro +
+            "Fecha: " + fechaSolicitud
+  }
+}
